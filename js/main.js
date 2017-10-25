4f5f14c820a4ac94da76ed22f7d1651df7a6007f
Harry Hunt <developingharry@outlook.com>
2017-10-25 20:14:26 +0100

    pane experiment

--- js/main.js
+++ js/main.js
@@ -4,4 +4,5 @@ function viewModel() {
         this.listIsVisible(!this.listIsVisible());
     };
 }
+
 ko.applyBindings(viewModel);
--- js/map-maker.js
+++ js/map-maker.js
@@ -8,4 +8,13 @@ $.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCRZhDuPRPkI-pUsZ3
         zoom: 18,
         mapTypeControl: false
     });
+    var myLatlng = new google.maps.LatLng(-50.8207025,-0.1353432);
+    var marker = new google.maps.Marker({
+    position : myLatlng,
+    title:"Hello World!"
 });
+marker.setMap(map);
+});
+
+
+// To add the marker to the map, call setMap();
